0dc37e96558bcb896e8ec45ee8d4cd0c824d675c
mvlasov <mykytavlasov@gmail.com>
2018-09-08 12:57:29 +0200

    change id from root to app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import "./styles.css";
-import App from "./components/App";
+import './styles.css';
+import App from './components/App';
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('app');
 ReactDOM.render(<App />, rootElement);
